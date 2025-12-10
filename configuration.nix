911383b96eb86bf43f0d1edd54e0f059a4aba33c
Dominic Evans <8060970+dnwe@users.noreply.github.com>
2025-12-10 22:11:46 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -43,33 +43,5 @@
 
   networking.useDHCP = lib.mkDefault true;
 
-  disko.devices.disk1 = {
-    device = lib.mkDefault "/dev/mmcblk0";
-    type = "disk";
-    content = {
-      type = "gpt";
-      partitions = {
-        ESP = {
-          type = "EF00";
-          size = "500M";
-          content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = "/boot";
-            mountOptions = [ "umask=0077" ];
-          };
-        };
-        root = {
-          size = "100%";
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/";
-          };
-        };
-      };
-    };
-  };
-
   system.stateVersion = "24.11";
 }
